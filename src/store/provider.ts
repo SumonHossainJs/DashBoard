8c9ba765d61b9d1a6e49e31aa7619005218d635a
SumonHossainJs <sumonhosainrakib@gmail.com>
2024-04-15 21:25:51 +0300

    feat(store.js):Al hamdulilah

---
+++ src/store/provider.ts
@@ -0,0 +1,4 @@
+
+import React, { ReactNode} from "react";
+import { Provider } from "react-redux";
+import {store} from "./store";
---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, isAction, PayloadAction} from "@reduxjs/toolkit";
+
+
+interface UserState {
+    currentUser: any | null;
+    loading : boolean;
+    error: boolean;
+    
+}
+
+const initialState : UserState = {
+    currentUser: null,
+    loading: false,
+    error: false,
+};
+
+
+export const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        loginStart: (state) =>{
+            state.loading = true;
+        },
+        loginSuccess:(state, action: PayloadAction<any>) =>{
+            state.loading = false;
+            state.currentUser = action.payload;
+        },
+        loginFailure:(state) =>{
+            state.loading = false;
+            state.error = true;
+        },
+        logout:(state) =>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = false;
+        } 
+    }
+});
+
+
+export const { loginStart,loginFailure,loginSuccess,logout} = userSlice.actions;
+
+
+export default userSlice.reducer;
