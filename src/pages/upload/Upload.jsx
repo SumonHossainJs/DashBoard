a58b03d6d465ebf736e6761e1c025f775c0c2ad3
SumonHossainJs <sumonhosainrakib@gmail.com>
2025-04-11 00:15:03 +0300

    upload fixed

--- src/pages/upload/Upload.jsx
+++ src/pages/upload/Upload.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import "./Upload.scss";
+import "./upload.scss";
 import axios from "axios";
 import Items from "./elements/Items";
 import { showSuccessAlert } from "../../Utils/Alert";
