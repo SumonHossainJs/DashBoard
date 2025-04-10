4550539de395720c761c2fc37ee782d9caf77af3
SumonHossainJs <sumonhosainrakib@gmail.com>
2025-04-11 00:10:55 +0300

    request url changed

--- src/Utils/newRequest.js
+++ src/Utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-    baseURL:"http://localhost:3030/",
+    baseURL:"https://a4codebackend.onrender.com",
     withCredentials:true,
 });
 
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./Menu.scss";
+import "./menu.scss";
 import { menu } from "../../data";
 
 const Menu = () => {
