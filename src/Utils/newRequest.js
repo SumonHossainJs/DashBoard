a7e2ffb152f9cf314188f483dadb4d63eb737fd6
SumonHossainJs <sumonhosainrakib@gmail.com>
2025-06-29 20:45:05 +0300

    new Depolyment

--- src/Utils/newRequest.js
+++ src/Utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-    baseURL:"https://a4codebackend.onrender.com",
+    baseURL:"https://a4codebackend-1.onrender.com",
     withCredentials:true,
 });
 
